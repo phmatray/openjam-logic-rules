2bd75e79e05487724de24bf4e434a72864f1afd9
Philippe Matray <phmatray@gmail.com>
2019-02-15 14:52:35 +0100

    Add wallaby.js

---
+++ wallaby.js
@@ -0,0 +1,15 @@
+module.exports = function(wallaby) {
+  return {
+    files: ['src/**/*.ts?(x)', '!src/**/*.spec.ts?(x)'],
+    tests: ['src/**/*.spec.ts?(x)'],
+
+    env: {
+      type: 'node',
+      runner: 'node'
+    },
+
+    testFramework: 'jest',
+
+    debug: true
+  };
+};
